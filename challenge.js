63609aef4efcdacd657ee5a6c51cecac7a007c6f
omaran bazna <omran4d@gmail.com>
2022-08-19 21:40:07 -0400

    add test2

--- challenge.js
+++ challenge.js
@@ -15,3 +15,18 @@ and the function should output 6. Here are the six pairs of adjacent hedges in t
  [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]
 
 */
+
+test2 = [
+  [1, 0, 0, 0],
+  [1, 1, 0, 0],
+  [1, 0, 0, 1],
+];
+years2 = 2;
+
+/*
+the final matrix will be:
+[[1, 1, 1, 1],
+ [1, 0, 1, 1],
+ [1, 1, 1, 1]]
+
+ */
