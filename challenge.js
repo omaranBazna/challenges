89a0aa8bc5451bfa5ac6b9b429cb82ab8fcfe015
omaran bazna <omran4d@gmail.com>
2022-08-19 21:39:14 -0400

    add test1 result

--- challenge.js
+++ challenge.js
@@ -1,7 +1,17 @@
+function fun(num, mat) {}
 
+test1Mat1 = [
+  [0, 0, 1],
+  [0, 0, 0],
+];
+years1 = 1;
+/*
 
-const fun(num,mat){
+the final matrix will be:
+[[0, 1, 1],
+ [0, 1, 1]]
+and the function should output 6. Here are the six pairs of adjacent hedges in this garden:
+[[0, 1, 1],    [[0, 1, 1],    [[0, 1, 1],    [[0, 1, 1],    [[0, 1, 1],    [[0, 1, 1],
+ [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]     [0, 1, 1]]
 
-
-    
-}
+*/
